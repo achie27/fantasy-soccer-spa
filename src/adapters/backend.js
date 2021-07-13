3acc2fc4942e0a81ff19bdc20922414eb47c508c
ME <architmathur2011@gmail.com>
2021-07-13 20:14:54 +0530

    feat: partially implemets the login form

---
+++ src/adapters/backend.js
@@ -0,0 +1,3 @@
+export const getUserToken = async (email, password) => {
+  return Promise.resolve({});
+};
--- src/components/LoginBox.js
+++ src/components/LoginBox.js
@@ -1,5 +1,38 @@
+import { TextField, Button } from "@material-ui/core";
+import { getUserToken } from "../adapters/backend";
+import { setLoggedInUser } from "../contexts/login";
+
 function LoginBox() {
-  return <div>yeeeehaw</div>;
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const { accessToken = "" } = await getUserToken(
+      e.target.elements.email,
+      e.target.elements.password
+    );
+    setLoggedInUser(accessToken);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <TextField
+        id="email"
+        name="email"
+        label="Email"
+        variant="outlined"
+        type="text"
+        autoFocus
+      />
+      <TextField
+        id="password"
+        name="password"
+        label="Password"
+        variant="outlined"
+        type="password"
+      />
+      <Button type="submit">Login</Button>
+    </form>
+  );
 }
 
 export default LoginBox;
